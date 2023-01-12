32819fb6c3c74e015a3d5c6e4e72fc8e175e5e4d
Ethan Willers <willers.enw@gmail.com>
2023-01-12 15:54:25 -0800

    yoinked code from previous challenge

---
+++ index.js
@@ -0,0 +1,55 @@
+const inquirer = require('inquirer');
+const fs = require('fs');
+
+const questions = [
+    {
+        type: 'input',
+        name: 'title',
+        message: 'Enter Project Title:'
+    },
+    {
+        type: 'input',
+        message: 'Enter Project Description:',
+        name: 'description'
+    },
+    {
+        type: 'editor',
+        name: 'install',
+        message: 'Enter Installation Instructions:'
+    },
+    {
+        type: 'editor',
+        name: 'usage',
+        message: 'Enter Usage Instructions:'
+    },
+    {
+        type: 'editor',
+        name: 'contribution',
+        message: 'Enter Contribution Guidelines:'
+    },
+    {
+        type: 'editor',
+        name: 'test',
+        message: 'Enter Test Instructions:'
+    },
+    {
+        type: 'list',
+        name: 'license',
+        message: 'Pick a license:',
+        choices: ['ISC', 'MIT']
+    }
+];
+
+inquirer
+    .prompt(questions)
+    .then((answers) => {
+        const readmeText = buildReadme(answers.title, answers.description, answers.install, answers.usage, answers.contribution, answers.test, answers.license);
+        fs.writeFileSync(`db/README.md`, readmeText);
+    })
+    .catch((error) => {
+        if (error.isTtyError) {
+            // Prompt couldn't be rendered in the current environment
+          } else {
+            // Something else went wrong
+          }
+    });
