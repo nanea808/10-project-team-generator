96a81fcb3a89c9f12d77fed30eed9499b9411a33
Ethan Willers <willers.enw@gmail.com>
2023-01-14 18:51:25 -0800

    manager questions array filled

--- index.js
+++ index.js
@@ -4,17 +4,46 @@ const { Employee, Manager, Engineer, Intern } = require('./lib/classes.js');
 
 const employees = [];
 
-const questions1 = [
+const managerQuestions = [
     // Prompt for managers name, employee ID, email address, and office number
+    {
+        type: 'input',
+        name: 'ManagerName',
+        message: "What's the managers full name?"
+    },
+    {
+        type: 'input',
+        name: 'ManagerEmployeeID',
+        message: "What's the managers employee ID?"
+    },
+    {
+        type: 'input',
+        name: 'ManagerEmail',
+        message: "What's the managers email address?"
+    },
+    {
+        type: 'input',
+        name: 'ManagerOfficeNum',
+        message: "What's the managers office number?"
+    }
+];
+
+const engineerQuestions = [
+    // Prompt for engineer’s name, ID, email, and GitHub username
 
 ];
 
-const questions2 = [
+const internQuestions = [
+    // Prompt for intern’s name, ID, email, and school
+
+];
+
+const engIntRecursive = [
     // Prompt to add engineer or intern
     {
         type: 'list',
         name: 'internOrEngineer',
-        message: "Would you like to add an intern or engineer to the team?",
+        message: "Would you like to add an engineer or intern to the team?",
         choices: [
             'Engineer',
             'Intern',
@@ -24,7 +53,7 @@ const questions2 = [
     }
 ];
 
-inquirer.prompt(questions1).then((answers) => {
+inquirer.prompt(managerQuestions).then((answers) => {
     // Make Manager object
 
     // Push object to employees array
@@ -33,21 +62,22 @@ inquirer.prompt(questions1).then((answers) => {
 });
 
 function ask() {
-    inquirer.prompt(questions2).then((answers) => {
-        const nextTimeMessage = 'Would you like to add another intern or engineer to the team?'
+    inquirer.prompt(engIntRecursive).then((answers) => {
+        const nextTimeMessage = 'Would you like to add another engineer or intern to the team?'
         if (answers.internOrEngineer === 'Engineer') {
-            // Create Engineer object
-
-            // Push object to employees array
-
-            questions2[0].message = nextTimeMessage;
+            inquirer.prompt(engineerQuestions).then((answers) => {
+                // Create Engineer object
+                
+                // Push object to employees array
+            });
+            engIntRecursive[0].message = nextTimeMessage;
             ask();
         } else if (answers.internOrEngineer === 'Intern') {
             // Create Intern object
 
             // Push object to employees array
-
-            questions2[0].message = nextTimeMessage;
+            
+            engIntRecursive[0].message = nextTimeMessage;
             ask();
         } else {
             // exit
