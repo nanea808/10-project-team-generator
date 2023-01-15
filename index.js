333cbcb85b859177653637ced756963efcf9c6ec
Ethan Willers <willers.enw@gmail.com>
2023-01-14 20:20:14 -0800

    engineer and intern question arrays complete

--- index.js
+++ index.js
@@ -8,34 +8,72 @@ const managerQuestions = [
     // Prompt for managers name, employee ID, email address, and office number
     {
         type: 'input',
-        name: 'ManagerName',
+        name: 'managerName',
         message: "What's the managers full name?"
     },
     {
         type: 'input',
-        name: 'ManagerEmployeeID',
+        name: 'managerEmployeeID',
         message: "What's the managers employee ID?"
     },
     {
         type: 'input',
-        name: 'ManagerEmail',
+        name: 'managerEmail',
         message: "What's the managers email address?"
     },
     {
         type: 'input',
-        name: 'ManagerOfficeNum',
+        name: 'managerOfficeNum',
         message: "What's the managers office number?"
     }
 ];
 
 const engineerQuestions = [
     // Prompt for engineer’s name, ID, email, and GitHub username
-
+    {
+        type: 'input',
+        name: 'engineerName',
+        message: "What's the Engineer's full name?"
+    },
+    {
+        type: 'input',
+        name: 'engineerEmployeeID',
+        message: "What's the Engineer's employee ID?"
+    },
+    {
+        type: 'input',
+        name: 'engineerEmail',
+        message: "What's the Engineer's email address?"
+    },
+    {
+        type: 'input',
+        name: 'engineerGitHub',
+        message: "What's the Engineer's GitHub username?"
+    }
 ];
 
 const internQuestions = [
     // Prompt for intern’s name, ID, email, and school
-
+    {
+        type: 'input',
+        name: 'internName',
+        message: "What's the Intern's full name?"
+    },
+    {
+        type: 'input',
+        name: 'internEmployeeID',
+        message: "What's the Intern's employee ID?"
+    },
+    {
+        type: 'input',
+        name: 'internEmail',
+        message: "What's the Intern's email address?"
+    },
+    {
+        type: 'input',
+        name: 'internSchool',
+        message: "What's the Intern's school?"
+    }
 ];
 
 const engIntRecursive = [
@@ -58,6 +96,7 @@ inquirer.prompt(managerQuestions).then((answers) => {
 
     // Push object to employees array
 
+    console.log('\n');
     ask();
 });
 
@@ -69,18 +108,24 @@ function ask() {
                 // Create Engineer object
                 
                 // Push object to employees array
-            });
-            engIntRecursive[0].message = nextTimeMessage;
-            ask();
-        } else if (answers.internOrEngineer === 'Intern') {
-            // Create Intern object
 
-            // Push object to employees array
-            
-            engIntRecursive[0].message = nextTimeMessage;
-            ask();
+                console.log('\n');
+                engIntRecursive[0].message = nextTimeMessage;
+                ask();
+            });
+        } else if (answers.internOrEngineer === 'Intern') {
+            inquirer.prompt(internQuestions).then((answers) => {
+                // Create Intern object
+                
+                // Push object to employees array
+
+                console.log('\n');
+                engIntRecursive[0].message = nextTimeMessage;
+                ask();
+            });
         } else {
             // exit
+            console.log('\nSending HTML data!')
         }
     });
 }
